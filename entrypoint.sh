eb0561fcd031072e455b36de3d08162c16bb656b
Barak Michener <me@barakmich.com>
2021-04-05 18:30:15 -0700

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ DIST_PROCESS=${DIST_PROCESS:-"server"}
 HOSTADDR=${HOSTADDR:-""}
 PORT=${PORT:-""}
 CONFIG_FILE=${CONFIG_FILE:-"/config/${DIST_PROCESS}.conf"}
-OUTPUT_CONFIG_FILE=${CONFIG_FILE:-"/etc/sccache-${DIST_PROCESS}.conf"}
+OUTPUT_CONFIG_FILE=${OUTPUT_CONFIG_FILE:-"/etc/sccache-${DIST_PROCESS}.conf"}
 
 VALID=1
 
@@ -31,8 +31,10 @@ if [ -z "$HOSTADDR" ]; then
 fi
 
 cp $CONFIG_FILE $OUTPUT_CONFIG_FILE
-sed -i "s!\$\$HOSTADDR\$\$!${HOSTADDR}!g" $OUTPUT_CONFIG_FILE
-sed -i "s!\$\$PORT\$\$!${PORT}!g" $OUTPUT_CONFIG_FILE
+sed -i 's!\$\$HOSTADDR\$\$!'"${HOSTADDR}"'!g' $OUTPUT_CONFIG_FILE
+sed -i 's!\$\$PORT\$\$!'"${PORT}"'!g' $OUTPUT_CONFIG_FILE
+
+cat $OUTPUT_CONFIG_FILE
 
 /usr/local/bin/sccache-dist $DIST_PROCESS --config $OUTPUT_CONFIG_FILE
 
